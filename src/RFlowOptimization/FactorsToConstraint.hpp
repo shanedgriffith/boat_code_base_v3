19216cf9a66050831d82d6f173f8a5289e785ef3
Shane <griffith.shane@gmail.com>
2017-08-31 16:08:47 +0200

    factors to Constraint

---
+++ src/RFlowOptimization/FactorsToConstraint.hpp
@@ -0,0 +1,45 @@
+//
+//  FactorsToConstraint.hpp
+//
+//  Created by Shane Griffith on 8/31/17.
+//  Copyright (c) 2017 shane. All rights reserved.
+//
+
+#ifndef SRC_RFLOWOPTIMIZATION_FACTORSTOCONSTRAINT_HPP_
+#define SRC_RFLOWOPTIMIZATION_FACTORSTOCONSTRAINT_HPP_
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <vector>
+
+#include <FileParsing/FileParsing.hpp>
+
+#include "LocalizedPoseData.hpp"
+
+class FactorsToConstraint: public FileParsing{
+protected:
+static const std::string _locoptname;
+
+void ProcessLineEntries(int type, std::vector<std::string>& lp){}
+void ReadDelimitedFile(std::string file, int type){}
+public:
+std::string _path;
+
+HopcountLog(std::string basepath):
+_path(basepath) {}
+
+static bool LogExists(std::string basepath, std::string hopdate);
+double LoadHopDistance(std::string hopdate);
+std::vector<double> LoadPriorRerror(std::string hopdate, int count);
+std::vector<double> GetAvgHopCounts();
+void SaveLocalLog(std::string hopdate, int numverified, std::vector<LocalizedPoseData>& localizations, std::vector<double> lpd_rerror);
+};
+
+
+
+
+#endif /* SRC_RFLOWOPTIMIZATION_FACTORSTOCONSTRAINT_HPP_ */
